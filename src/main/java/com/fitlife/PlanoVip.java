c430f6e4bf9dbc9ba0c81bd1e5947a1dbb51e77e
Airton Rodrigues <jarodriguesj1@gmail.com>
2025-11-08 04:37:12 -0300

    criação do plano vip

---
+++ src/main/java/com/fitlife/PlanoVip.java
@@ -0,0 +1,28 @@
+package com.fitlife;
+
+public class PlanoVip extends Aluno implements PlanosFit {
+    @Override
+    public String NomePlano() {
+        return "============== PLANO MENSAL =================" +
+                "Olá " + getNome() +
+                "essas são a as caractetisticas do seu plano na nossa academia " +
+                "VALOR = 1200" +
+                "HORÁRIOS MUSCULAÇÃO= 24H" +
+                "DIREITO DE MODALIDADES DE AULA DISTINTAS  = ";
+    }
+
+    @Override
+    public double getValorDoPlano() {
+        return 2640;
+    }
+
+    @Override
+    public int getDuracaDeDias() {
+        return 365;
+    }
+
+    @Override
+    public boolean getPodeReservarArea() {   //esperando interface de modalidades
+        return true;
+    }
+}
