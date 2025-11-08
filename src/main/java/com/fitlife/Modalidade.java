205017f863594c259522bb51f2f351ca03f47d60
PedroJacquesDEV <pedro.jacques.academico@gmail.com>
2025-11-08 02:50:30 -0300

    Adicionando a classe Modalidade

---
+++ src/main/java/com/fitlife/Modalidade.java
@@ -0,0 +1,34 @@
+package com.fitlife;
+
+public class Modalidade {
+    private String id;
+    private String nome;
+    private String descricao;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+}
