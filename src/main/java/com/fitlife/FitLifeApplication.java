07b943df10514c6afddaecebfb561ad09d1f2634
surufel <henriquefrancasmm@gmail.com>
2025-11-13 21:54:37 -0300

    Conexão com SQL (localhost)

--- src/main/java/com/fitlife/FitLifeApplication.java
+++ src/main/java/com/fitlife/FitLifeApplication.java
@@ -2,10 +2,25 @@ package com.fitlife;
 
 import com.fitlife.Aluno.Aluno;
 import com.fitlife.Professor.Professor;
-import com.fitlife.Database.ConexaoSQL;
+
+import java.sql.*;
+
 
 public class FitLifeApplication {
     public static void main(String[] args) {
+        // CONEXÃO COM O MYSQL (EM LOCALHOST)
+        try{
+            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/fitlife","root","1234");
 
+            Statement statement = conexao.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("username"));
+                System.out.println(resultSet.getString("password"));
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
