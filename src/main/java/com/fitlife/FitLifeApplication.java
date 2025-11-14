e7015ce54293e5f498c97c8d43a3be60abd740a6
surufel <henriquefrancasmm@gmail.com>
2025-11-13 21:26:15 -0300

    add import ConexaoSQL

--- src/main/java/com/fitlife/FitLifeApplication.java
+++ src/main/java/com/fitlife/FitLifeApplication.java
@@ -2,6 +2,7 @@ package com.fitlife;
 
 import com.fitlife.Aluno.Aluno;
 import com.fitlife.Professor.Professor;
+import com.fitlife.Database.ConexaoSQL;
 
 public class FitLifeApplication {
     public static void main(String[] args) {
