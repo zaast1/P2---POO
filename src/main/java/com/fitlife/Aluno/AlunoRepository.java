b274ddcc20023bef57a5f4bed341752b37ba6e28
surufel <henriquefrancasmm@gmail.com>
2025-11-13 21:19:22 -0300

    Remove Springboot

--- src/main/java/com/fitlife/Aluno/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fitlife.Aluno;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
