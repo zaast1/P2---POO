3d53eda4d2efd428b4c60cc08aa1bf33dd00e5b6
Airton Rodrigues <jarodriguesj1@gmail.com>
2025-11-17 15:48:02 -0300

    criação do plano vip e remoção das classes DAO

--- src/main/java/com/fitlife/Plano/PlanosFit.java
+++ src/main/java/com/fitlife/Plano/PlanosFit.java
@@ -1,12 +1,15 @@
 package com.fitlife.Plano;
 
 
-
+import com.fitlife.Aluno.Aluno;
 
 //criando ionterface para os planos da academia
 public interface PlanosFit {
     String NomePlano();          //nome do plano escolhido na classe
-                                 //deve retoranar os valores e informações do plano, como
+
+    String detalharPlano(Aluno aluno);
+
+    //deve retoranar os valores e informações do plano, como
                                  //plano mensal ==> 120 por mês acesso à musculação em tais horarios...
     double getValorDoPlano();
     int getDuracaDeDias();
